c3814a2a08f636c60da518b8b0d7fd9a5ef9da02
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-08-03 13:28:07 -0400

    minor styling stuff

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import '../../styles/Login.css';
-import '../../styles/bootstrap.min.css';
+//import '../../styles/Login.css';
+//import '../../styles/bootstrap.min.css';
 
 export default function() {
     const [message, setMessage] = useState("");
--- src/pages/GuestPages/LoginPage/LoginPage.tsx
+++ src/pages/GuestPages/LoginPage/LoginPage.tsx
@@ -3,9 +3,6 @@ import Login from "../../../components/Auth/Login"
 export const LoginPage: React.FC = () => {
 
     return(
-        <>
-        <h3>Login Page</h3>
         <Login></Login>
-        </>
     )
 }
