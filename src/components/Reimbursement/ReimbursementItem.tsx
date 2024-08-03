407a8f1eb983e87f98dd3b017c226e2a7f2d57d4
Mohammad Kabakibi <Mohammad.2001k@hotmail.com>
2024-08-03 13:07:19 -0400

    added storing the token

--- src/components/Reimbursement/ReimbursementItem.tsx
+++ src/components/Reimbursement/ReimbursementItem.tsx
@@ -1,1 +1,11 @@
-export {}
+import { User } from "../User/UserProfile";
+
+export interface Reimbursement {
+    reimbId?:number,
+    description?:string,
+    amount?:number,
+    status?:string,
+    user?:User,
+    createdAt?:string,
+    resolvedAt?:string
+}
--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,1 +1,13 @@
-export {}
+export interface User {
+    userId:number,
+    firstName:string,
+    lastName:string,
+    username:string,
+    password:string,
+    role:Role
+}
+
+interface Role{
+    roleId:number,
+    name:string
+}
