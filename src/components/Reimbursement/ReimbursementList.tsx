4de7bce759d715c9e50dc3a5b0cbfd7f948b3fa9
Mohammad Kabakibi <Mohammad.2001k@hotmail.com>
2024-08-04 02:03:38 -0400

    added reimb item and list

--- src/components/Reimbursement/ReimbursementList.tsx
+++ src/components/Reimbursement/ReimbursementList.tsx
@@ -1,1 +1,16 @@
-export {}
+import { Col, Row } from "react-bootstrap";
+import { ReimbursementInterface } from "../Interfaces/ReimbursementInterface";
+import ReimbursementItem from "./ReimbursementItem";
+
+export default (reimbursements:{reimbursementsList?:ReimbursementInterface[]})=>{
+    
+    return(
+        <Row xs={1} md={3} className="g-5">
+        {reimbursements.reimbursementsList?.map((reimbursement) => (
+            <Col key={reimbursement.reimbId}>
+                <ReimbursementItem {...reimbursement} />
+            </Col>
+        ))}
+        </Row>
+    );
+}
--- src/pages/ManagerPages/ReimbursementPage/ReimbursementPage.tsx
+++ src/pages/ManagerPages/ReimbursementPage/ReimbursementPage.tsx
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react"
-import reimbursementService from "../../../services/reimbursementService";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import ReimbursementList from "../../../components/Reimbursement/ReimbursementList";
+import { ReimbursementInterface } from "../../../components/Interfaces/ReimbursementInterface";
 
 
 export const Reimbursementpage: React.FC = () => {
 
-    const [reimbursements, setReimbursements] = useState([]);
+    const [reimbursements, setReimbursements] = useState<{reimbursementsList?:ReimbursementInterface[]}>({});
     const navigate = useNavigate();
     
     useEffect(()=>{
-        reimbursementService.getReimbursementsByUserID(2).then((r)=>{
-            if(r) // not null
-                setReimbursements(r);
-            else
-                navigate('/login')
-        });
+        let token = localStorage.getItem('token');
+        if(token){
+            const api = axios.create({
+                baseURL: 'http://localhost:8080',
+                headers: {'Authorization': 'Bearer '+token}
+            });
+            api.get('/reimbursements')
+                .then((response)=>{
+                    setReimbursements({reimbursementsList:response.data});
+                })
+                .catch((error) =>{
+                    if(error.response?.status == 401)
+                        navigate('../login');
+                });
+        }
+        else
+            navigate('../login');
     },[]);
 
     return(
         <>
         <h3>Manager Reimbursements Page</h3>
-        {reimbursements.map((q)=>q['reimbId']+"  ")}
-        <br/>{reimbursements.length}
+        <ReimbursementList {...reimbursements} />
         </>
     )
 }
