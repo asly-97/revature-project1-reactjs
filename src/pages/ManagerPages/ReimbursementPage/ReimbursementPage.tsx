8da6b09ea3844ece65e8ef8a33c51386aa8f2fad
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-08-04 13:52:16 -0400

    filter by status

--- src/pages/ManagerPages/ReimbursementPage/ReimbursementPage.tsx
+++ src/pages/ManagerPages/ReimbursementPage/ReimbursementPage.tsx
@@ -1,15 +1,24 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { __api_url } from "../../../utils/constants"
-import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import { Button, ButtonGroup, Card, Col, Container, Row, ToggleButton } from "react-bootstrap"
 import MReimbursementItem from "../../../components/Reimbursement/ManagerReimbursement/MReimbursementItem"
 import { ReimbursementInterface } from "../../../components/Interfaces/ReimbursementInterface"
 
+
+const enum StatusFilter{
+    All,
+    Pending,
+    Approved,
+    Denied
+}
 
 
 export const ReimbursementPage: React.FC = () => {
     
     const[reimbursements,setReimbursements] = useState([])
+
+    const[statusFilter,setStatusFilter] = useState<StatusFilter>(StatusFilter.All)
 
 
     const _token = localStorage.getItem('token')
@@ -24,7 +33,7 @@ export const ReimbursementPage: React.FC = () => {
     })
 
     useEffect(() => {
-        getAllReimbursements()
+        getAllReimbursementsByStatus(StatusFilter.All)
     },[])
 
     
@@ -34,8 +43,8 @@ export const ReimbursementPage: React.FC = () => {
     },[reimbursements])
 
 
-    const getAllReimbursements = async () => {
-        await api.get('/reimbursements')
+    const getAllReimbursementsByPath = async (path:string) => {
+        await api.get(`/reimbursements${path}`)
                 .then( ({data}) => {
                     console.log('-- data -- ',data)
                     setReimbursements(data)
@@ -53,7 +62,7 @@ export const ReimbursementPage: React.FC = () => {
                     console.log('-- response -- ',response)
 
                     //reload the reimbursements
-                    getAllReimbursements()
+                    getAllReimbursementsByStatus(StatusFilter.All)
                 })
                 .catch(err => {
                     console.log('-- error -- ',err)
@@ -61,11 +70,79 @@ export const ReimbursementPage: React.FC = () => {
     }
 
 
+    const getAllReimbursementsByStatus = (status:StatusFilter) => {
+        setStatusFilter(status)
+        if(status == StatusFilter.All){
+            getAllReimbursementsByPath('')
+        }
+        else if(status == StatusFilter.Pending){
+            getAllReimbursementsByPath('/pending')
+        }
+        else if(status == StatusFilter.Approved){
+            getAllReimbursementsByPath('/approved')
+        }
+        else if(status == StatusFilter.Denied){
+            getAllReimbursementsByPath('/denied')
+        }
+    }
+
+
     return(
         <>
             <Container fluid>
 
-                <Row xs={1} md={3} className="g-3">
+                <Row className="justify-content-center mb-4">
+                    <Col xs={4}>
+                    <ButtonGroup>
+                        <ToggleButton
+                            id={'all'}
+                            type="radio"
+                            variant={'outline-info'}
+                            name="radio"
+                            value={StatusFilter.All}
+                            checked={statusFilter == StatusFilter.All}
+                            onChange={e=>getAllReimbursementsByStatus(StatusFilter.All)}
+                        >
+                            All
+                        </ToggleButton>
+                        <ToggleButton
+                            id={'pending'}
+                            type="radio"
+                            variant={'outline-secondary'}
+                            name="radio"
+                            value={StatusFilter.Pending}
+                            checked={statusFilter == StatusFilter.Pending}
+                            onChange={e=>getAllReimbursementsByStatus(StatusFilter.Pending)}
+                        >
+                            Pending
+                        </ToggleButton>
+                        <ToggleButton
+                            id={'approved'}
+                            type="radio"
+                            variant={'outline-success'}
+                            name="radio"
+                            value={StatusFilter.Approved}
+                            checked={statusFilter == StatusFilter.Approved}
+                            onChange={e=>getAllReimbursementsByStatus(StatusFilter.Approved)}
+                        >
+                            Approved
+                        </ToggleButton>
+                        <ToggleButton
+                            id={'denied'}
+                            type="radio"
+                            variant={'outline-danger'}
+                            name="radio"
+                            value={StatusFilter.Denied}
+                            checked={statusFilter == StatusFilter.Denied}
+                            onChange={e=>getAllReimbursementsByStatus(StatusFilter.Denied)}
+                        >
+                            Denied
+                        </ToggleButton>
+                    </ButtonGroup>
+                    </Col>
+                </Row>
+
+                <Row xs={2} md={3} className="g-5">
 
                 {
 
