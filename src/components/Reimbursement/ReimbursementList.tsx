48f3acc6a0bdc29c1270222edbe96b253b8d8dd6
Mohammad Kabakibi <Mohammad.2001k@hotmail.com>
2024-08-07 13:48:23 -0400

    changed reimbursements list to grid

--- src/components/Reimbursement/ReimbursementList.tsx
+++ src/components/Reimbursement/ReimbursementList.tsx
@@ -1,20 +1,16 @@
-import { CardGroup, Col, Row } from "react-bootstrap";
 import { ReimbursementInterface } from "../../Interfaces/ReimbursementInterface";
 import ReimbursementItem from "./ReimbursementItem";
+import '../../styles/reimbursementList.css'
 
 export default (reimbursements:{reimbursementsList?:ReimbursementInterface[]})=>{
-
-    let length = reimbursements.reimbursementsList? reimbursements.reimbursementsList.length : 0;
     
     return(
-        <CardGroup>
-            <Row xs={1} md={ length > 3? 3: length} className="g-3" style={{justifyItems:"center"}}>
+        <div className="cards_container">
             {reimbursements.reimbursementsList?.map((reimbursement) => (
-                        <Col key={reimbursement.reimbId}>
-                            <ReimbursementItem {...reimbursement} />
-                        </Col>
+                <div>
+                    <ReimbursementItem {...reimbursement} />
+                </div>
             ))}
-            </Row>
-        </CardGroup>
+        </div>
     );
 }
